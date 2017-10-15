e9c32656318a11fcd7c591e1b4914b5e9b974cc1
emailman <emailman@dtcc.edu>
2017-10-15 16:21:38 -0400

    Added option to remove an item from inventory

--- CIS209-Struct_Demo/Form1.Designer.cs
+++ CIS209-Struct_Demo/Form1.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbxMake = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbxInventory = new System.Windows.Forms.ListBox();
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.btnRemoveCar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxYear);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbxMake);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(202, 119);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Car Information";
+            // 
+            // tbxMake
+            // 
+            this.tbxMake.Location = new System.Drawing.Point(94, 20);
+            this.tbxMake.Name = "tbxMake";
+            this.tbxMake.Size = new System.Drawing.Size(100, 20);
+            this.tbxMake.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Make:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Year:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Location = new System.Drawing.Point(94, 52);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(100, 20);
+            this.tbxYear.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mileage:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // lbxInventory
+            // 
+            this.lbxInventory.FormattingEnabled = true;
+            this.lbxInventory.Location = new System.Drawing.Point(263, 30);
+            this.lbxInventory.Name = "lbxInventory";
+            this.lbxInventory.Size = new System.Drawing.Size(169, 95);
+            this.lbxInventory.TabIndex = 1;
+            // 
+            // btnAddCar
+            // 
+            this.btnAddCar.Location = new System.Drawing.Point(72, 151);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(75, 41);
+            this.btnAddCar.TabIndex = 2;
+            this.btnAddCar.Text = "Add Car to List";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            this.btnAddCar.Click += new System.EventHandler(this.btnAddCar_Click);
+            // 
+            // btnRemoveCar
+            // 
+            this.btnRemoveCar.Location = new System.Drawing.Point(292, 151);
+            this.btnRemoveCar.Name = "btnRemoveCar";
+            this.btnRemoveCar.Size = new System.Drawing.Size(75, 41);
+            this.btnRemoveCar.TabIndex = 3;
+            this.btnRemoveCar.Text = "Remove Car from List";
+            this.btnRemoveCar.UseVisualStyleBackColor = true;
+            this.btnRemoveCar.Click += new System.EventHandler(this.btnRemoveCar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(239, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 119);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Car Inventory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(453, 222);
+            this.Controls.Add(this.btnRemoveCar);
+            this.Controls.Add(this.btnAddCar);
+            this.Controls.Add(this.lbxInventory);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Car Inventory";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxMake;
+        private System.Windows.Forms.ListBox lbxInventory;
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.Button btnRemoveCar;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- CIS209-Struct_Demo/Form1.cs
+++ CIS209-Struct_Demo/Form1.cs
@@ -1,20 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace CIS209_Struct_Demo
 {
+    // Define a struct for an automobile
+    struct Automobile
+    {
+        public string make;
+        public int year;
+        public double mileage;
+    }
+
     public partial class Form1 : Form
     {
+        // Create a class list for automobiles
+        private List<Automobile> carList = new List<Automobile>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Method to get the data from the form and 
+        // set the properties of the instance
+        private bool GetData(ref Automobile car)
+        {
+            try
+            {
+                car.make = tbxMake.Text;
+                car.year = int.Parse(tbxYear.Text);
+                car.mileage = double.Parse(textBox2.Text);
+                return true;           
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Check input fields for missing or invalid data",
+                    "Data Entry Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void btnAddCar_Click(object sender, EventArgs e)
+        {
+            // Create a new car instance
+            Automobile newCar = new Automobile();
+
+            // Get the properties of the new car
+            // from the form.  If there is an error,
+            // don't add the item
+            if (GetData(ref newCar))
+            {
+                // Add the new car to the list
+                carList.Add(newCar);
+
+                // Clear the form
+                tbxMake.Clear();
+                tbxYear.Clear();
+                textBox2.Clear();
+
+                // Reset the focus
+                tbxMake.Focus();
+
+                // Refresh the inventory list box
+                refreshInventoryList();
+            }
+        }
+
+        private void btnRemoveCar_Click(object sender, EventArgs e)
+        {
+            // Get the index of the selected item
+            int selectedCar = lbxInventory.SelectedIndex;
+
+            // Was something selected?
+            if (selectedCar >= 0)
+            {
+                // Confirm it's OK to remove the item
+                DialogResult result = MessageBox.Show("OK to Delete " + lbxInventory.SelectedItem.ToString(),
+                    "Confirm Delete ", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    // Remove it from the list
+                    carList.RemoveAt(selectedCar);
+
+                    // Refresh the inventory list box
+                    refreshInventoryList();
+                }
+            }
+        }
+
+        private void refreshInventoryList()
+        {
+            // Clear the inventory list box
+            lbxInventory.Items.Clear();
+
+            // Show the car list in the inventory list
+            foreach (Automobile each in carList)
+            {
+                string output = each.year + " " + each.make +
+                    " with " + each.mileage + " miles ";
+
+                lbxInventory.Items.Add(output);
+            }
         }
     }
 }
